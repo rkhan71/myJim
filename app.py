e78025d15f17ea35fe52a61dd7c19cb485f664a3
Rayan Khan <rayan.ahkhan@gmail.com>
2022-07-05 15:53:22 +0300

    allows users to login and logout

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from email import message
 from flask import Flask, render_template, redirect, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -6,6 +7,16 @@ import sqlite3
 app = Flask(__name__)
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route("/")
 def index():
@@ -15,9 +26,9 @@ def index():
 def register():
     if request.method == "POST":
         if not request.form.get("username") or not request.form.get("password") or not request.form.get("confirm"):
-            return redirect("error.html")
+            return render_template("error.html", message="Required field not filled out.")
         if request.form.get("password") != request.form.get("confirm"):
-            return redirect("error.html")
+            return render_template("error.html", message="Passwords do not match.")
         connect = sqlite3.connect("myJim.db")
         cursor = connect.cursor()
         cursor.execute("SELECT username FROM users")
@@ -25,7 +36,7 @@ def register():
         unames = cursor.fetchall()
         for i in unames:
             if i == uname:
-                return redirect("error.html")
+                return render_template("error.html", message="Username is already taken.")
         pword = generate_password_hash(request.form.get("password"))
         cursor.execute("INSERT INTO users (username, hash) VALUES (?, ?)", (uname, pword))
         connect.commit()
@@ -33,3 +44,26 @@ def register():
         return redirect("/")
     else:
         return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+    if request.method == "POST":
+        if not request.form.get("username") or not request.form.get("password"):
+            return render_template("error.html", message="Required field not filled out.")
+        connect = sqlite3.connect("myJim.db")
+        cursor = connect.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = ?", (request.form.get("username"),))
+        userdata = cursor.fetchone()
+        if not userdata or not check_password_hash(userdata[2], request.form.get("password")):
+            return render_template("error.html", message="Invalid username and/or password.")
+        session["user_id"] = userdata[0]
+        connect.close()
+        return redirect("/")
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
