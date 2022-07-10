f531ebd472f8dc398911bd66ab6583976b4b6d72
Rayan Khan <rayan.ahkhan@gmail.com>
2022-07-10 17:02:18 +0300

    diary page defaults to current date, cleaner code

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
+from datetime import date
 
 app = Flask(__name__)
 
@@ -57,6 +58,7 @@ def login():
         if not userdata or not check_password_hash(userdata[2], request.form.get("password")):
             return render_template("error.html", message="Invalid username and/or password.")
         session["user_id"] = userdata[0]
+        session["date"] = date.today().strftime("%Y-%m-%d")
         connect.close()
         return redirect("/")
     else:
@@ -69,19 +71,18 @@ def logout():
 
 @app.route("/diary", methods=["GET", "POST"])
 def diary():
+    connect = sqlite3.connect("myJim.db")
+    cursor = connect.cursor()
+    cursor.execute("SELECT * FROM exercise_list WHERE user_id = ?", (session["user_id"],))
+    exercises = cursor.fetchall()
+    cursor.execute("SELECT * FROM diary WHERE user_id = ? AND day = ?", (session["user_id"], session["date"]))
+    entries = cursor.fetchall()
     if request.method == "POST":
         if request.form.get("exercise"):
             exercise = request.form.get("exercise")
-            connect = sqlite3.connect("myJim.db")
-            cursor = connect.cursor()
             cursor.execute("INSERT INTO exercise_list VALUES (?, ?)", (session["user_id"], exercise))
             cursor.execute("SELECT * FROM exercise_list WHERE user_id = ?", (session["user_id"],))
             exercises = cursor.fetchall()
-            cursor.execute("SELECT * FROM diary WHERE user_id = ? AND day = ?", (session["user_id"], session["date"]))
-            entries = cursor.fetchall()
-            connect.commit()
-            connect.close()
-            return render_template("diary.html", exercises=exercises, entries=entries)
         elif request.form.get("exercise-select"):
             if not request.form.get("weight"):
                 return render_template("error.html", message="Required field not filled out.")
@@ -104,40 +105,20 @@ def diary():
                 return render_template("error.html", message="Invalid entry.")
             if units not in ["kg", "lbs"]:
                 return render_template("error.html", message="Invalid entry.")
-            connect = sqlite3.connect("myJim.db")
-            cursor = connect.cursor()
             data = (session["user_id"], exercise, weight, units, sets, reps, session["date"])
             cursor.execute("INSERT INTO diary (user_id, exercise, weight, units, sets, reps, day) VALUES (?, ?, ?, ?, ?, ?, ?)", data)
-            cursor.execute("SELECT * FROM exercise_list WHERE user_id = ?", (session["user_id"],))
-            exercises = cursor.fetchall()
             cursor.execute("SELECT * FROM diary WHERE user_id = ? AND day = ?", (session["user_id"], session["date"]))
             entries = cursor.fetchall()
-            connect.commit()
-            connect.close()
-            return render_template("diary.html", exercises=exercises, entries=entries)
         elif request.form.get("id"):
-            connect = sqlite3.connect("myJim.db")
-            cursor = connect.cursor()
             cursor.execute("DELETE FROM diary WHERE id = ?", (request.form.get("id"),))
-            cursor.execute("SELECT * FROM exercise_list WHERE user_id = ?", (session["user_id"],))
-            exercises = cursor.fetchall()
             cursor.execute("SELECT * FROM diary WHERE user_id = ? AND day = ?", (session["user_id"], session["date"]))
             entries = cursor.fetchall()
-            connect.commit()
-            connect.close()
-            return render_template("diary.html", exercises=exercises, entries=entries)
         elif request.form.get("exercise-del"):
-            connect = sqlite3.connect("myJim.db")
-            cursor = connect.cursor()
             cursor.execute("DELETE FROM exercise_list WHERE exercise = ?", (request.form.get("exercise-del"),))
             cursor.execute("SELECT * FROM exercise_list WHERE user_id = ?", (session["user_id"],))
             exercises = cursor.fetchall()
-            cursor.execute("SELECT * FROM diary WHERE user_id = ? AND day = ?", (session["user_id"], session["date"]))
-            entries = cursor.fetchall()
-            connect.commit()
-            connect.close()
-            return render_template("diary.html", exercises=exercises, entries=entries)
         else:
+            connect.close()
             return render_template("error.html", message="Required field not filled out.")
     else: 
         url = request.url
@@ -145,25 +126,11 @@ def diary():
         if len(temp) > 1:
             date = temp[1].split("=")
             session["date"] = date[1]
-            connect = sqlite3.connect("myJim.db")
-            cursor = connect.cursor()
-            cursor.execute("SELECT * FROM exercise_list WHERE user_id = ?", (session["user_id"],))
-            exercises = cursor.fetchall()
-            cursor.execute("SELECT * FROM diary WHERE user_id = ? AND day = ?", (session["user_id"], session["date"]))
-            entries = cursor.fetchall()
-            connect.close()
-            return render_template("diary.html", exercises=exercises, entries=entries)
-        elif "date" in session.keys():
-            connect = sqlite3.connect("myJim.db")
-            cursor = connect.cursor()
-            cursor.execute("SELECT * FROM exercise_list WHERE user_id = ?", (session["user_id"],))
-            exercises = cursor.fetchall()
-            cursor.execute("SELECT * FROM diary WHERE user_id = ? AND day = ?", (session["user_id"], session["date"]))
-            entries = cursor.fetchall()
-            connect.close()
-            return render_template("diary.html", exercises=exercises, entries=entries)
-        else:
-            return render_template("diary.html")
+        cursor.execute("SELECT * FROM diary WHERE user_id = ? AND day = ?", (session["user_id"], session["date"]))
+        entries = cursor.fetchall()
+    connect.commit()
+    connect.close()
+    return render_template("diary.html", exercises=exercises, entries=entries)
 
 @app.route("/progress")
 def progress():
